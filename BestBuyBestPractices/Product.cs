c85a98d961fb3250e551426ac385b0500e7ff9ce
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2023-03-09 13:11:51 -0700

    Completed Exercise 1&2

---
+++ BestBuyBestPractices/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BestBuyBestPractices
+{
+	public class Product
+	{
+		public Product()
+		{
+		}
+
+		public int ProductID { get; set; }
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public string OnSale { get; set; }
+		public string StockLevel { get; set; }
+
+	}
+}
+
